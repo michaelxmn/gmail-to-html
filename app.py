19b51786c5900c8d2cb48b01978768193164af37
michael <michael.cnxmn@gmail.com>
2024-02-03 09:41:55 +0800

    provide label from cli

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ import click
 @click.command()
 @click.option('--credentials', type=str, default = 'credentials.json', help='Credentials on gcp, you can login `https://console.cloud.google.com/apis/credentials`, create your credentials and then save it to local file such as my_credentials.json, pass `--credentials my_credentials.json`')
 @click.option('--output_dir', type=str, default = 'output', help='Directory to place *.html')
-def main(credentials, output_dir):
+@click.option('--label', type=str, default = 'Inbox', help='e.g. Inbox')
+def main(credentials, output_dir, label):
   validate_credentials(credentials)
   create_output_directory(output_dir)
   gmail_connection = GmailConnection(credentials_file=credentials)
   gmail_reader = GmailReader(output_dir=output_dir, gmail_api_service = gmail_connection.connect())
-  gmail_reader.read(label_name = 'Certification')
+  gmail_reader.read(label_name = label)
 
 def validate_credentials(credentials):
   if not os.path.exists(credentials):
--- gmail_reader.py
+++ gmail_reader.py
@@ -18,17 +18,29 @@ class GmailReader:
   def read(self, label_name):
     try:
       search_query = f'label:{label_name}'
-      # Make a request to list messages based on the search query
       results = self.service.users().messages().list(userId='me', q=search_query).execute()
       messages = results.get('messages', [])
 
       if not messages:
-        print(f'No messages found with the label "{label_name}".')
+        print(f'No messages found with the query "{search_query}".')
       else:
-        print(f'Messages with the label "{label_name}":')
+        print(f'Messages with the query "{search_query}":')
         self.transform_emails_to_html(self.service, label_name, messages)
     except Exception as e:
       print(f'An error occurred: {e}')
+
+  def transform_emails_to_html(self, service, label_name, messages):
+    os.makedirs(f'{self.output_dir}/{label_name}', exist_ok=True)
+    with multiprocessing.Pool(8) as pool:
+      processing_args = []
+      for message in messages:
+        processing_args.append((service, label_name, message))
+      partial_function = partial(invoke_instance_method, self)
+      stopwatch = Stopwatch()
+      stopwatch.start()
+      pool.map(partial_function, processing_args)
+      stopwatch.stop()
+      print(f"Elapsed Time: {stopwatch.elapsed_time()} seconds")
 
   def transform_to_html(self, service, label_name, message):
     msg = service.users().messages().get(userId='me', id=message['id']).execute()
@@ -46,19 +58,6 @@ class GmailReader:
     mail_file_name = f"{self.output_dir}/{label_name}/{mail.subject}.html"
     with open(mail_file_name, "w") as mail_file:
       mail_file.write(mail.format_to_html())
-
-  def transform_emails_to_html(self, service, label_name, messages):
-    os.makedirs(f'{self.output_dir}/{label_name}', exist_ok=True)
-    with multiprocessing.Pool(8) as pool:
-      processing_args = []
-      for message in messages:
-        processing_args.append((service, label_name, message))
-      partial_function = partial(invoke_instance_method, self)
-      stopwatch = Stopwatch()
-      stopwatch.start()
-      pool.map(partial_function, processing_args)
-      stopwatch.stop()
-      print(f"Elapsed Time: {stopwatch.elapsed_time()} seconds")
 
   def read_mail_body_by_walking_tree(self, service, mail, payload):
     if payload.get('mimeType') == 'text/html':
